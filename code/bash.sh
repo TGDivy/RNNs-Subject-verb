d607acc9273d5229988992fcd78ba9d5e0983e3e
Divy Bramhecha <tgdivy@gmail.com>
2020-10-04 17:51:56 +0100

    Final Results

---
+++ code/bash.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# set the STRING variable
+STRING="Hello World!"
+# print the contents of the variable on screen
+echo $STRING
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 0 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 0 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 0 0.05
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 2 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 2 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 2 0.05
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 5 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 5 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 25 5 0.05
+
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 0 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 0 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 0 0.05
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 2 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 2 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 2 0.05
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 5 0.5
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 5 0.1
+#python3 rnn.py train-lm /../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data 50 5 0.05
+#---------------------
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 0 0.5
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 2 0.5
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 5 0.5
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 0 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 2 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 5 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 0 0.05
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 2 0.05
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 50 5 0.05
+
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 0 0.5
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 2 0.5
+#python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 5 0.5
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 0 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 2 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 5 0.1
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 0 0.05
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 2 0.05
+# python3 rnn.py train-np "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 25 5 0.05
+#--------------
+#python3 rnn.py predict-lm "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data"
+# #-------------------------------------------------
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 0
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 1
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 2
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 3
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 4
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 5
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 6
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 7
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 8
+# python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 9
+python3 rnn.py "Q5" "/../../mnt/c/Users/divyb/OneDrive/Documents/Uni/NLU/CourseWork1/data" 10
